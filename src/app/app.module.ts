f16a759ee9bd00387b61392535d2dc5dfd845323
Octavio Santana <osq.005@gmail.com>
2023-11-09 18:16:30 +0000

    end section 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +12,11 @@ import { SharedModule } from './shared/shared.module';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
     SharedModule,
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments = {
+  baseUrl: 'https://url.produccion/api'
+}
